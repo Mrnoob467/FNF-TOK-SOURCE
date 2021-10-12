eda6fc102fdacb52f41e00e03a65b3e451656f0d
lemonking99 <84978816+lemonking99@users.noreply.github.com>
2021-10-11 20:29:25 -0400

    Final Charts (thx Disc <3)

--- source/Main.hx
+++ source/Main.hx
@@ -73,12 +73,7 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		#if cpp
-		initialState = Caching;
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-		#else
-		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-		#end
 
 		addChild(game);
 
